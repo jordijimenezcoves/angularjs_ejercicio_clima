51018a0a9c2a2f1d685eea860dfdfd4e15af61cc
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2021-07-11 12:43:02 +0200

    function use in interpolation example

--- js/app.js
+++ js/app.js
@@ -2,8 +2,6 @@
 
 var app = angular.module('ejemplosApp',[ ]);
 
-
-
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
   //http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&callback=JSON_CALLBACK
@@ -14,15 +12,14 @@ app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
 
 			$scope.clima = data;
 
-
 		});
 
-
+	$scope.kelvinCelsius = function(k){
+		c = k - 273.15;
+		c = Math.round( c * 100 ) / 100;
+		return c; 
+	};
 
 }]);
-
-
-
-
 
 })();
